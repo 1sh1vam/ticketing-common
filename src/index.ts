d3be15efa59af56bd1bc59945ee530d526f55741
1sh1vam <shivam07.mt@gmail.com>
2022-03-20 13:46:08 +0530

    importing and exporting all files

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-interface Color {
-    red: number,
-    green: number,
-    blue: number,
-}
+// Importing and exporting all the files
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-const color : Color = {
-    red: 123,
-    green: 245,
-    blue: 123
-}
-
-console.log(color);
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
